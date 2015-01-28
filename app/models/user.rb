6a5cdd06e797292c8311a3cda056396ad2842e26
ummerr <ummerr@gmail.com>
2015-01-28 17:21:28 -0500

    added a validation for user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_many :goals
   has_many :expenditures
 
+  validates :name, presence: true
+
   def age
   now = Time.now.utc.to_date
   now.year - self.birthday.year - (self.birthday.to_date.change(:year => now.year) > now ? 1 : 0)
