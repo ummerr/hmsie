2df8d4ef6b9032c03c2f012f67c39dc0cf8c2053
ummerr <ummerr@gmail.com>
2015-01-28 16:42:29 -0500

    small tweaks to UI

--- app/controllers/profile.rb
+++ app/controllers/profile.rb
@@ -6,27 +6,4 @@ get '/profile' do
   end
 end
 
-post '/goal' do
-  @goal = Goal.create(params[:goal])
-  current_user.goals << @goal
 
-  if request.xhr?
-    erb :"/profile/_goal", layout: false
-  else
-    redirect "/profile"
-  end
-
-end
-
-post '/expenditure' do
-  @expenditure = Expenditure.create(params[:expenditure])
-  current_user.expenditures << @expenditure
-
-  if request.xhr?
-    erb :"/profile/_expenditure", layout: false
-  else
-    redirect "/profile"
-  end
-
-
-end
