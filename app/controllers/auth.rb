2d901fb1d27b93def06be8f2e92d45b860f19767
ummerr <ummerr@gmail.com>
2015-01-28 10:53:06 -0500

    updated auth routes for posting from sign up and login

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -3,9 +3,37 @@ get '/' do
 end
 
 get '/login' do
-  
+
+end
+
+post '/login' do
+  user = User.find_by(name: params[:user][:name])
+
+  if user.try(:authenticate, params[:user][:password])
+    session[:user_id] = user.id
+    redirect '#'
+  else
+    redirect '/'
+  end
 end
 
 get '/signup' do
-  
+
+end
+
+post '/signup' do
+  user = User.create(params[:user])
+
+  if user.save
+    session[:user_id] = user.id
+    redirect '#'
+  else
+    redirect '/'
+  end
+
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/'
 end
