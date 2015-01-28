f6ea208ef94ef8e942dd554289925abc90b2f714
ummerr <ummerr@gmail.com>
2015-01-28 10:19:27 -0500

    updated expenditure model and association

--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -1,3 +1,15 @@
 class Expenditure < ActiveRecord::Base
-  # Remember to create a migration!
+  before_save :calculate_resting_expenditure
+  belongs_to :user
+
+  private
+
+  def calculate_resting_expenditure
+    if self.gender.downcase == 'male' || self.gender.downcase = 'm'
+      self.resting_expenditure = (10 * self.weight.to_i) + (6.25 * self.height.to_i) - (5 * self.age.to_i) + 5
+    else
+      self.resting_expenditure = (10 * self.weight.to_i) + (6.25 * self.height.to_i) - (5 * self.age.to_i) - 161
+    end
+  end
+
 end
