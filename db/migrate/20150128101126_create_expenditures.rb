b044cffa9be607249d89681dd4a57f3bf818f897
ummerr <ummerr@gmail.com>
2015-01-28 10:15:16 -0500

    updated create_expenditure migration

--- db/migrate/20150128101126_create_expenditures.rb
+++ db/migrate/20150128101126_create_expenditures.rb
@@ -1,4 +1,16 @@
 class CreateExpenditures < ActiveRecord::Migration
   def change
+    create_table :expenditures do |t|
+      t.references :user
+
+      t.string :age
+      t.string :weight
+      t.string :height
+      t.string :gender
+
+      t.string :resting_expenditure
+
+      t.timestamps
+    end
   end
 end
